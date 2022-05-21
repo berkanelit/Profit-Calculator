9c145ba355feaf8f150bd8d8f45acf1a9efc4b7a
Berkan Başer <68708902+berkanelit@users.noreply.github.com>
2022-05-22 00:59:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ayı Giriniz");
+            int aylik = int.Parse(Console.ReadLine());
+            Console.WriteLine("Paranızı Giriniz");
+            int para = int.Parse(Console.ReadLine());
+            Console.WriteLine("Karı Yüzdesini Giriniz");
+            int kar = int.Parse(Console.ReadLine());
+            int toplam;
+            for (int i = 0; i <= aylik; i++)
+            {
+                toplam = para * kar / 100;
+                para = toplam + para;
+            }
+            Console.WriteLine("Yıl Sonu Kar Marjınız : " + para);
+            Console.ReadLine();
+        }
+    }
+    
+}
